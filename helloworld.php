f4fc40ecaea5cf3d06aaeda5817ee1690d87aafd
imhaijie <40633683+imhaijie@users.noreply.github.com>
2018-07-12 11:22:30 +0800

    v1.3-gb2312

--- helloworld.php
+++ helloworld.php
@@ -1,6 +1,7 @@
 
 <?php
-header("content-type:text/html;charset=utf-8");  //è®¾ç½®ç¼–ç 
+header("content-type:text/html;charset=GB2312");  //ÉèÖÃ±àÂë
 $url="http://rmrb.applinzi.com/rmrb-1.php";
 echo readfile($url);
+echo "**/"
 ?>
